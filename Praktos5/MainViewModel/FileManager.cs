708e5c89e488b5140507592874fe4382967a251c
maksv1 <isip_m.v.vinogorov@mpt.ru>
2024-05-10 01:28:57 +0300

    Практос 5 (2 сем.)

---
+++ Praktos5/MainViewModel/FileManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Praktos5
+{
+    public class Figure
+    {
+        public string Name { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Figure() { }
+
+        public Figure(string name, double width, double height)
+        {
+            Name = name;
+            Width = width;
+            Height = height;
+        }
+    }
+
+    public class FileManager
+    {
+        private List<Figure> figures;
+
+        public FileManager()
+        {
+            figures = new List<Figure>();
+        }
+
+        public List<Figure> Figures
+        {
+            get { return figures; }
+        }
+
+        public List<Figure> LoadFromFile(string filePath)
+        {
+            string fileExtension = Path.GetExtension(filePath);
+
+            switch (fileExtension)
+            {
+                case ".txt":
+                    LoadFromTxt(filePath);
+                    break;
+                case ".json":
+                    LoadFromJson(filePath);
+                    break;
+                case ".xml":
+                    LoadFromXml(filePath);
+                    break;
+                default:
+                    Console.WriteLine("Неподдерживаемый формат файла.");
+                    break;
+            }
+
+            return figures;
+        }
+
+        private void LoadFromTxt(string filePath)
+        {
+        }
+
+        private void LoadFromJson(string filePath)
+        {
+        }
+
+        private void LoadFromXml(string filePath)
+        {
+        }
+
+        public void SaveToFile(string filePath, List<Figure> figures)
+        {
+            string fileExtension = Path.GetExtension(filePath);
+
+            switch (fileExtension)
+            {
+                case ".txt":
+                    SaveToTxt(filePath, figures);
+                    break;
+                case ".json":
+                    SaveToJson(filePath, figures);
+                    break;
+                case ".xml":
+                    SaveToXml(filePath, figures);
+                    break;
+                default:
+                    Console.WriteLine("Неподдерживаемый формат файла для сохранения.");
+                    break;
+            }
+        }
+
+        private void SaveToTxt(string filePath, List<Figure> figures)
+        {
+        }
+
+        private void SaveToJson(string filePath, List<Figure> figures)
+        {
+        }
+
+        private void SaveToXml(string filePath, List<Figure> figures)
+        {
+        }
+    }
+}
---
+++ Praktos5/View/MainViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows.Documents;
+
+namespace Praktos5
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _filePath; 
+        private string _savePath; 
+        private ObservableCollection<Figure> _figures; 
+        private FileManager _fileManager; 
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set
+            {
+                _filePath = value;
+                OnPropertyChanged(nameof(FilePath));
+            }
+        }
+
+        public string SavePath
+        {
+            get { return _savePath; }
+            set
+            {
+                _savePath = value;
+                OnPropertyChanged(nameof(SavePath));
+            }
+        }
+
+        public ObservableCollection<Figure> Figures
+        {
+            get { return _figures; }
+            set
+            {
+                _figures = value;
+                OnPropertyChanged(nameof(Figures));
+            }
+        }
+
+        public ICommand LoadFileCommand { get; }
+
+        public ICommand SaveFileCommand { get; }
+
+        public MainViewModel()
+        {
+            _fileManager = new FileManager();
+            Figures = new ObservableCollection<Figure>();
+
+            LoadFileCommand = new Praktos5.RelayCommand(LoadFile);
+            SaveFileCommand = new Praktos5.RelayCommand(SaveFile);
+        }
+
+        private void LoadFile(object obj)
+        {
+            if (!string.IsNullOrEmpty(FilePath))
+            {
+                var figuresList = _fileManager.LoadFromFile(FilePath);
+                if (figuresList != null)
+                {
+                    Figures.Clear();
+                    foreach (var figure in figuresList)
+                    {
+                        Figures.Add(figure);
+                    }
+                }
+            }
+        }
+
+        private void SaveFile(object obj)
+        {
+            if (!string.IsNullOrEmpty(SavePath))
+            {
+                _fileManager.SaveToFile(SavePath, Figures.ToList());
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
